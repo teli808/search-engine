d6ad5694694361bbd2871d040082cd5dda10fe51
Tyler Li <teli@uci.edu>
2019-12-14 23:04:40 -1000

    updated UI

---
+++ user_interface.py
@@ -0,0 +1,9 @@
+import tkinter
+
+def create_UI():
+    window = tkinter.Tk()
+    window.title("Zootgle Search")
+    label = tkinter.Label(window, text = "Welcome to Zootgle!").pack()
+    window.mainloop()
+
+create_UI()
